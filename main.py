0582c202a01ea1f8d498039576f0c09cd5f3047a
Alex Ainea <xo.ainea@gmail.com>
2024-12-23 20:10:12 +0300

    centered the screen

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from tkinter import *
 
 root = Tk()
-root.title = "CALCULATOR"
+root.title("CALCULATOR")
+
+window_width = 400
+window_height = 600
+
+
+# Get the screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Calculate the x and y position for the window to appear at the center
+x_pos = (screen_width // 2) - (window_width // 2)
+y_pos = (screen_height // 2) - (window_height // 2)
+
+root.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
+
+main_frame = Frame(root, bg="white", width=window_width, height=window_height)
+main_frame.pack()
 
 root.mainloop()
